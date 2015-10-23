b83a8d98289bd78a5b115e4d33ce7cb2378029ce
eleanorstrib <eleanor@sloan.mit.edu>
2015-10-22 20:22:34 -0700

    added VC's for other views

---
+++ planets/QuizVC.swift
@@ -0,0 +1,14 @@
+//
+//  QuizVC.swift
+//  planets
+//
+//  Created by Eleanor Stribling on 10/22/15.
+//  Copyright © 2015 eleanorstrib. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class QuizVC: UITableViewController {
+    //codes
+}
