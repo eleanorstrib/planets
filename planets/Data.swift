cc1d0c729aad25f191734c1c5b9366132e3b39d8
eleanorstrib <eleanor@sloan.mit.edu>
2015-10-26 20:24:17 -0700

    added more to data file, will check approach -- not sure if there's a better way to do this

--- planets/Data.swift
+++ planets/Data.swift
@@ -11,16 +11,18 @@ import UIKit
 
 struct Planet {
     let name: String
+    let imageName: String
     let nickname: String
     let fromSun: String
-    let daysInYear: Int
+    let daysInYear: String
     let atmosphere: String
-    let temperature: Int
+    let temperature: String
     let status: String
     let group: String
     let funFact: String
     
     static let nameKey = "nameKey"
+    static let imageName = "imageName"
     static let nicknameKey = "nicknameKey"
     static let fromSunKey = "fromSunKey"
     static let daysInYearKey = "daysInYearKey"
@@ -29,4 +31,17 @@ struct Planet {
     static let statusKey = "statusKey"
     static let groupKey = "groupKey"
     static let funFactKey = "funFactKey"
+    
+    init(dictionary: [String : String]) {
+        self.name = dictionary[Planet.nameKey]!
+        self.imageName = dictionary[Planet.imageName]!
+        self.nickname = dictionary[Planet.nicknameKey]!
+        self.fromSun = dictionary[Planet.fromSunKey]!
+        self.daysInYear = dictionary[Planet.daysInYearKey]!
+        self.atmosphere = dictionary[Planet.atmosphereKey]!
+        self.temperature = dictionary[Planet.temperatureKey]!
+        self.status = dictionary[Planet.statusKey]!
+        self.group = dictionary[Planet.groupKey]!
+        self.funFact = dictionary[Planet.funFactKey]!
+    }
 }
